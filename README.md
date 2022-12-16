d268fd76c31cf321520931f67e11f44a4d295f16
Woji <wojciechpiskorz@gmail.com>
2022-12-16 23:24:59 +0100

    newspaper-homepage-start

--- README.md
+++
@@ -1,6 +0,0 @@
-# frontend-mentor-projects
-All projects for frontend mentor I decided to give a try
-
-My main goal with this is to get used to two things. Nuxt 3 and vue 3 with help of tailwind css
-
-Have fun me!
---
+++ news-homepage-main/code/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+
+
+
+</script>
+
+<template>
+  <section>
+    <div class="container mx-auto py-8 flex justify-center items-center h-screen bg-slate-50">
+      <h1 class="text-4xl font-bold">Sample</h1>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
